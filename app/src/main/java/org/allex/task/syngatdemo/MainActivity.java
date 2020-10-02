4ed53d491f1730c5820d3ce4a158dbab135913b7
William Alexander <william.al1379@gmail.com>
2020-10-02 16:04:20 -0600

    Add dependecies and import database

--- app/src/main/java/org/allex/task/syngatdemo/MainActivity.java
+++ app/src/main/java/org/allex/task/syngatdemo/MainActivity.java
@@ -2,6 +2,8 @@ package org.allex.task.syngatdemo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.content.Context;
+import com.couchbase.lite.DatabaseConfiguration;
 
 public class MainActivity extends Activity {
 
@@ -9,6 +11,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Context context = getApplicationContext();
+        DatabaseConfiguration config = new DatabaseConfiguration(context);
     }
 }
