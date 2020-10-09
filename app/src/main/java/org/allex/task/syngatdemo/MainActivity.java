94e942e47233d522d50477fc7d8bea50de4c8662
VenturaBrandon <brandon.ventura@acomi.coop>
2020-10-08 20:51:57 -0600

    Elimina código innecesario

--- app/src/main/java/org/allex/task/syngatdemo/MainActivity.java
+++ app/src/main/java/org/allex/task/syngatdemo/MainActivity.java
@@ -30,82 +30,10 @@ import com.couchbase.lite.ReplicatorConfiguration;
 
 public class MainActivity extends Activity {
 
-    Database database;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Context context = getApplicationContext();
 
-        // Get the database (and create it if it doesn’t exist).
-        DatabaseConfiguration config = new DatabaseConfiguration(context);
-        try {
-            database = new Database(BuildConfig.DB, config);
-        } catch (CouchbaseLiteException e) {
-            Log.e("error", e.getMessage());
-        }
-
-
-        // Create a new document (i.e. a record) in the database.
-        MutableDocument mutableDoc = new MutableDocument()
-                .setString("name", "Tarea desde Android")
-                .setBoolean("status", true);
-        Toast.makeText(this, mutableDoc.getId(), Toast.LENGTH_SHORT).show();
-        // Save it to the database.
-        try {
-            database.save(mutableDoc);
-            Log.i("success", "Datos guardados");
-        } catch (CouchbaseLiteException e) {
-            Log.e("Error", "Error al guardar");
-        }
-
-        // Create a query to fetch documents of type SDK.
-        Query query = QueryBuilder.select(SelectResult.expression(Meta.id),
-                SelectResult.property("name"),
-                SelectResult.property("status"))
-                .from(DataSource.database(database))
-                .where(Expression.property("status").equalTo(Expression.booleanValue(true)));
-
-
-        try {
-            ResultSet result = query.execute();
-            for (Result r : result) {
-                Log.i("success", r.getString(0));
-                Log.i("success", r.getString("name"));
-                Log.i("success", String.valueOf(r.getBoolean("status")));
-                Log.i("success", "-----------------------------");
-            }
-        } catch (Exception e) {
-            Log.e("error", e.getMessage());
-        }
-
-
-        // Create replicators to push and pull changes to and from the cloud.
-
-        Endpoint targetEndpoint = null;
-        try {
-            targetEndpoint = new URLEndpoint(new URI(BuildConfig.ENDPOINT));
-        } catch (URISyntaxException e) {
-            Log.e("error", e.getMessage());
-        }
-
-        ReplicatorConfiguration replConfig = new ReplicatorConfiguration(database, targetEndpoint);
-        replConfig.setReplicatorType(ReplicatorConfiguration.ReplicatorType.PUSH_AND_PULL);
-        // Add authentication.
-        replConfig.setAuthenticator(new BasicAuthenticator(BuildConfig.USER, BuildConfig.PASSWORD));
-
-        // Create replicator (be sure to hold a reference somewhere that will prevent the Replicator from being GCed)
-        Replicator replicator = new Replicator(replConfig);
-
-        // Listen to replicator change events.
-        replicator.addChangeListener(change -> {
-            if (change.getStatus().getError() != null) {
-                Log.i("error", "Error code ::  " + change.getStatus().getError().getCode());
-            }
-        });
-
-// Start replication.
-        replicator.start();
     }
 }
