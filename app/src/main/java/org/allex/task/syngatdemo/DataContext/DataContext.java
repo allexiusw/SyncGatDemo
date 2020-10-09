46811a2af5600a7d47bdfe30fbdcfae8037c3879
VenturaBrandon <brandon.ventura@acomi.coop>
2020-10-08 22:05:25 -0600

    Agrega contexto de datos. Elimina librerias innecesarias de Actividad

---
+++ app/src/main/java/org/allex/task/syngatdemo/DataContext/DataContext.java
@@ -0,0 +1,71 @@
+package org.allex.task.syngatdemo.DataContext;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.couchbase.lite.BasicAuthenticator;
+import com.couchbase.lite.CouchbaseLiteException;
+import com.couchbase.lite.Database;
+import com.couchbase.lite.DatabaseConfiguration;
+import com.couchbase.lite.Endpoint;
+import com.couchbase.lite.Replicator;
+import com.couchbase.lite.ReplicatorConfiguration;
+import com.couchbase.lite.URLEndpoint;
+
+import org.allex.task.syngatdemo.BuildConfig;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class DataContext {
+    private Database database;
+    private static DataContext instance = null;
+
+    public Database getDatabase() {
+        return database;
+    }
+
+    protected DataContext(Context context) {
+
+        //Obtiene la base de datos o la crea si no existe
+        DatabaseConfiguration dbConfig = new DatabaseConfiguration(context);
+        try{
+            database = new Database(BuildConfig.DB, dbConfig);
+        }catch (CouchbaseLiteException ex){
+            Log.e("error", ex.getMessage());
+        }
+
+        //Crea la URI del recurso a manejar
+        Endpoint endpoint = null;
+        try{
+            endpoint = new URLEndpoint(new URI(BuildConfig.ENDPOINT));
+        }catch (URISyntaxException ex){
+            Log.e("error", ex.getMessage());
+        }
+
+        //Opciones de replicación de los datos
+        ReplicatorConfiguration replicatorConfiguration = new ReplicatorConfiguration(database, endpoint);
+        replicatorConfiguration.setReplicatorType(ReplicatorConfiguration.ReplicatorType.PUSH_AND_PULL);
+        replicatorConfiguration.setAuthenticator(new BasicAuthenticator(BuildConfig.USER, BuildConfig.PASSWORD));
+
+        //Crea el replicador de datos
+        Replicator replicator = new Replicator(replicatorConfiguration);
+
+        replicator.addChangeListener(change -> {
+           if(change.getStatus().getError() != null){
+               Log.e("error", "Error code: "+change.getStatus().getError().getCode());
+           }
+        });
+
+        //Inicia la replicación
+        replicator.start();
+    }
+
+
+    //Metodo que nos permite obtener una instacia de nuestro contexto de datos
+    public static DataContext getSharedDataContext(Context context){
+        instance = instance == null ? new DataContext(context) : instance;
+
+        return instance;
+    }
+}
--- app/src/main/java/org/allex/task/syngatdemo/MainActivity.java
+++ app/src/main/java/org/allex/task/syngatdemo/MainActivity.java
@@ -2,31 +2,6 @@ package org.allex.task.syngatdemo;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.content.Context;
-import android.util.Log;
-import android.widget.Toast;
-
-import com.couchbase.lite.CouchbaseLiteException;
-import com.couchbase.lite.DatabaseConfiguration;
-import com.couchbase.lite.Database;
-import com.couchbase.lite.Dictionary;
-import com.couchbase.lite.Meta;
-import com.couchbase.lite.MutableDocument;
-import com.couchbase.lite.Query;
-import com.couchbase.lite.QueryBuilder;
-import com.couchbase.lite.SelectResult;
-import com.couchbase.lite.DataSource;
-import com.couchbase.lite.Expression;
-import com.couchbase.lite.ResultSet;
-import com.couchbase.lite.Result;
-import com.couchbase.lite.URLEndpoint;
-import com.couchbase.lite.Endpoint;
-import java.net.URI;
-import java.net.URISyntaxException;
-import com.couchbase.lite.BasicAuthenticator;
-import com.couchbase.lite.Replicator;
-
-import com.couchbase.lite.ReplicatorConfiguration;
 
 public class MainActivity extends Activity {
 
