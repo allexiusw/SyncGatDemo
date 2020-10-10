047d05f9a6e5381317f558dbdb64a25181bd7c8f
VenturaBrandon <brandon.ventura@acomi.coop>
2020-10-09 18:31:03 -0600

    Agrega actividad de Creación de registros

---
+++ app/src/main/java/org/allex/task/syngatdemo/CrearPersonaActivity.java
@@ -0,0 +1,14 @@
+package org.allex.task.syngatdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CrearPersonaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_persona);
+        
+    }
+}
