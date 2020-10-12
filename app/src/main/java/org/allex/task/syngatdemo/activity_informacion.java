2f14c61f47302481b2790159e7bf20c9791b8e4b
Maryorihdz <alejandrahdz17200@gmail.com>
2020-10-12 01:57:42 -0600

    Actividad Informacion

---
+++ app/src/main/java/org/allex/task/syngatdemo/activity_informacion.java
@@ -0,0 +1,13 @@
+package org.allex.task.syngatdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_informacion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_informacion);
+    }
+}
