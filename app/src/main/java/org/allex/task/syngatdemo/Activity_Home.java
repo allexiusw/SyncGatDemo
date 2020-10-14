504aa00611075c76993b06c8ad3ae78c48b71716
VenturaBrandon <brandon.ventura@acomi.coop>
2020-10-13 22:31:43 -0600

    Corrige dependencias de AppCompat

--- app/src/main/java/org/allex/task/syngatdemo/Activity_Home.java
+++ app/src/main/java/org/allex/task/syngatdemo/Activity_Home.java
@@ -1,10 +1,11 @@
 package org.allex.task.syngatdemo;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Activity_Home extends AppCompatActivity {
 
