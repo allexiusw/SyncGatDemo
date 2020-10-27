4d8832bcd8ce6522f023207a3d69ae6d019ed8b5
VenturaBrandon <brandon.ventura@acomi.coop>
2020-10-27 15:28:41 -0600

    Mejora el manejo de canales para notificaciones
    -Agrega validacion de entrada de canal entrante si es nulo o no en el
    MessageService.
    -Agrega metodos de creacion de notificaciones para API mayor y menor a
    26 con el objetivo de separar las logicas.
    -Valida canal de administrador.

--- app/src/main/java/org/allex/task/syngatdemo/Utils/Util.java
+++ app/src/main/java/org/allex/task/syngatdemo/Utils/Util.java
@@ -6,8 +6,10 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.media.RingtoneManager;
 import android.os.Build;
+import android.util.Log;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -37,36 +39,53 @@ public class Util {
 
     public static void createNotification(Context context, int notificationId, Class activityClass,
                                           String notificationTitle, String notificationContent, String channelId){
-       if(channelId == context.getString(R.string.admin_notification_channel))
-           createAdminNotificationChannel(context);
-       else
-            createDefaultNotificationChannel(context);
+       //TODO: Restringir canal de admin para dispositivos mayores a API 26
         Intent intent = new Intent(context, activityClass);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
 
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-            NotificationCompat.Builder builder = new NotificationCompat.Builder(
-                    context, channelId)
-                    .setSmallIcon(R.drawable.ic_launcher_foreground)
-                    .setContentTitle(notificationTitle)
-                    .setContentText(notificationContent)
-                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                    .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
-                    .setAutoCancel(true)
-                    .setContentIntent(pendingIntent);
-            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
-            notificationManager.notify(notificationId, builder.build());
-        }else{
-            NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-            Notification.Builder builder = new Notification.Builder(context)
-                    .setSmallIcon(R.drawable.ic_launcher_foreground)
-                    .setContentTitle(notificationTitle)
-                    .setContentText(notificationContent)
-                    .setAutoCancel(true)
-                    .setContentIntent(pendingIntent);
-            notificationManager.notify(notificationId, builder.build());
-        }
+       if(channelId.equals(context.getString(R.string.admin_notification_channel))){
+           createAdminNotificationChannel(context);
+           createNotificationForHigherApi(context, notificationId, activityClass, notificationTitle,
+                   notificationContent, channelId, pendingIntent);
+       }else{
+           createDefaultNotificationChannel(context);
+           createNotificationForHigherApi(context, notificationId, activityClass, notificationTitle,
+                   notificationContent, channelId, pendingIntent);
+           if(Build.VERSION.SDK_INT < Build.VERSION_CODES.O)
+               createNotificationForLowerApi(context, notificationId, activityClass, notificationTitle,
+                       notificationContent, pendingIntent);
+       }
+   }
+
+   private static void createNotificationForHigherApi(Context context, int notificationId, Class activityClass,
+                                                      String notificationTitle, String notificationContent,
+                                                      String channelId, PendingIntent pendingIntent){
+       if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+           NotificationCompat.Builder builder = new NotificationCompat.Builder(
+                   context, channelId)
+                   .setSmallIcon(R.drawable.ic_launcher_foreground)
+                   .setContentTitle(notificationTitle)
+                   .setContentText(notificationContent)
+                   .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                   .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                   .setAutoCancel(true)
+                   .setContentIntent(pendingIntent);
+           NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+           notificationManager.notify(notificationId, builder.build());
+       }
+   }
+
+   private static void createNotificationForLowerApi(Context context, int notificationId, Class activityClass,
+                                                     String notificationTitle, String notificationContent, PendingIntent pendingIntent){
+       NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+       Notification.Builder builder = new Notification.Builder(context)
+               .setSmallIcon(R.drawable.ic_launcher_foreground)
+               .setContentTitle(notificationTitle)
+               .setContentText(notificationContent)
+               .setAutoCancel(true)
+               .setContentIntent(pendingIntent);
+       notificationManager.notify(notificationId, builder.build());
    }
 
     private static void createDefaultNotificationChannel(Context context){
@@ -76,6 +95,7 @@ public class Util {
             int importance = NotificationManager.IMPORTANCE_DEFAULT;
             NotificationChannel channel = new NotificationChannel(context.getString(R.string.default_notification_channel), name, importance);
             channel.setDescription(description);
+            channel.setLightColor(Color.GRAY);
             NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
             notificationManager.createNotificationChannel(channel);
         }
@@ -88,6 +108,7 @@ public class Util {
             int importance = NotificationManager.IMPORTANCE_HIGH;
             NotificationChannel channel = new NotificationChannel(context.getString(R.string.admin_notification_channel), name, importance);
             channel.setDescription(description);
+            channel.setLightColor(Color.GREEN);
             NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
             notificationManager.createNotificationChannel(channel);
         }
